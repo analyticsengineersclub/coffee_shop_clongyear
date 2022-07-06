4aef6e3d76c803c22c10ab6560c794aeb157dcee
Ubuntu <ubuntu@ip-172-31-27-76.us-east-2.compute.internal>
2022-07-06 22:08:21 +0000

    rename id columsn in staging models

--- models/staging/coffee_shop/stg_coffee_shop__order_items.sql
+++ models/staging/coffee_shop/stg_coffee_shop__order_items.sql
@@ -2,4 +2,8 @@ with order_items as (
     select * from {{ source('coffee_shop', 'order_items') }}
 )
 
-select * from order_items
+select
+    id as orderitem_id
+  , order_id
+  , product_id
+from order_items
